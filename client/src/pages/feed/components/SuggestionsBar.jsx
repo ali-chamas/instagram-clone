b06c971069efbfcf9f5c03cb317f7b1053ae6d27
watwatos <alichamas.22@hotmail.com>
2024-04-10 22:03:06 +0300

    started-suggestions-bar

--- client/src/pages/feed/components/SuggestionsBar.jsx
+++ client/src/pages/feed/components/SuggestionsBar.jsx
@@ -1,7 +1,33 @@
-import React from "react";
-
+import React, { useContext, useEffect } from "react";
+import { UserContext } from "../../../context/userContext";
+import { useNavigate } from "react-router-dom";
+import { sendRequest } from "../../../request-method/request";
 const SuggestionsBar = () => {
-  return <div className="suggestions">SuggestionsBar</div>;
+  const { user } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  const [suggestions, setSuggestions] = useState([]);
+
+  const fetchSuggestions = async () => {
+    const res = await sendRequest("GET", "/get-recommendations");
+    console.log(res);
+  };
+
+  useEffect(() => {
+    fetchSuggestions();
+  }, []);
+
+  return (
+    <div className="suggestion flex column gap">
+      <div className="w-full flex justify-between">
+        <p className="text-gray font-larger">Suggested for you</p>
+        <b className="cursor-pointer" onClick={() => navigate("/followings")}>
+          See All
+        </b>
+      </div>
+      <div></div>
+    </div>
+  );
 };
 
 export default SuggestionsBar;
--- client/src/pages/feed/page.jsx
+++ client/src/pages/feed/page.jsx
@@ -1,7 +1,13 @@
 import React from "react";
-
+import SuggestionsBar from "./components/SuggestionsBar";
+import "./style.css";
 const Feed = () => {
-  return <div className="flex"></div>;
+  return (
+    <div className="flex justify-between w-full p">
+      posts
+      <SuggestionsBar />
+    </div>
+  );
 };
 
 export default Feed;
